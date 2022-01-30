2b373e5a962d3e44d6604dd04c222b9e3fee1e80
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-01-30 14:12:10 +0100

    sandbox: rewritten to use lazy pages

--- packages/admin-sandbox/admin/pages/RandomPages.tsx
+++
@@ -1,5 +0,0 @@
-import { GenericPage } from '@contember/admin'
-
-export const Foo = <GenericPage>Hello from Foo</GenericPage>
-export const Bar = <GenericPage>Hello from Bar</GenericPage>
-export const Lorem = <GenericPage>Hello from Lorem</GenericPage>
--- packages/admin-sandbox/admin/pages/homepage.tsx
+++ packages/admin-sandbox/admin/pages/homepage.tsx
@@ -1,7 +1,7 @@
 import { EditPage } from '@contember/admin'
 import { ContentField } from '../components/ContentField'
 
-export const Homepage = (
+export default () => (
 	<EditPage entity="Homepage(unique = One)" setOnCreate="(unique = One)">
 		<ContentField field="content" />
 	</EditPage>
